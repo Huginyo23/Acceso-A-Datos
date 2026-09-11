9180625a91cd929823383cc457046e88bfddea4c
Huginyo23 <humama@alumnatflorida.es>
2026-09-11 09:18:40 +0200

    Update dades.java

--- dades.java
+++ dades.java
@@ -1,3 +1,7 @@
-System.out.println("Que le debe el Valencia a Franco");
-System.out.println("Porque Carlos Vidal es creyente judio y fan de Trakatra");
-System.out.println("Hola que tal soy Trakatra");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Que le debe el Valencia a Franco");
+        System.out.println("Porque Carlos Vidal es creyente judio y fan de Trakatra");
+        System.out.println("Hola que tal soy Trakatra");
+    }
+}
